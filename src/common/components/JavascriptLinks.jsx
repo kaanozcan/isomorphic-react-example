e393ebf1ea199292406eb6be86d5d951f57e3226
Kaan Özcan <kaan.ozcan@PT0004.local>
2016-02-08 17:03:29 +0200

    removed data releated parts

--- src/common/components/JavascriptLinks.jsx
+++ src/common/components/JavascriptLinks.jsx
@@ -4,48 +4,21 @@ import Links from "./Links"
 class JavascriptLinks extends Component {
   constructor(config){
     super(config);
-
-    const flux = this.flux = this.props.flux;
-
-    this.LinkStore = flux.getStore("LinkStore");
-    this.LinkActions = flux.getActions("LinkActions");
-
-    this.state = {
-      hotLinks: this.LinkStore.getState().hotLinks,
-      newLinks: this.LinkStore.getState().newLinks,
-      errLinks: this.LinkStore.getState().errLinks
-    }
-
-    this.handleStateChange = this.handleStateChange.bind(this);
   }
-
-  componentDidMount(){
-    this.LinkStore.listen(this.handleStateChange);
-  }
-
-  componentWillUnmount(){
-    this.LinkStore.unlisten(this.handleStateChange);
-  }
-
-  handleStateChange(data){
-    this.setState(Object.assign(this.state, data));
-  }
-
   render(){
-      const hotLinksData = this.state.hotLinks.data ? this.state.hotLinks.data.children : [];
-      const newLinksData = this.state.newLinks.data ? this.state.newLinks.data.children : [];
-
       return (
         <div>
-          <Links type="hot" data={hotLinksData}>
+          <Links type="hot">
             <h3>Hot Javascript Links</h3>
           </Links>
-          <Links type="new" data={newLinksData}>
+          <Links type="new">
             <h3>New Javascript Links</h3>
           </Links>
         </div>
       );
   }
 }
+
+
 
 export default JavascriptLinks
