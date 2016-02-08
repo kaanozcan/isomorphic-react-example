b806636263e78e79aa0daddcec9d92c92939e35f
Kaan Özcan <kaan.ozcan@PT0004.local>
2016-02-08 17:04:32 +0200

    implemented data fetching and error handling

--- src/common/components/Links/index.js
+++ src/common/components/Links/index.js
@@ -2,23 +2,73 @@ import React, { Component } from "react"
 import Link from "./Link"
 
 class Links extends Component {
-  render(){
-console.log("context", this.context.flux);
+  constructor(config, context){
+    super(config);
 
-    const links = this.props.data ? this.props.data.map((item, index) => {
-      return (<Link data={item.data} key={index}/>)
-    }) : null;
+    const flux = this.flux = context.flux;
+
+    this.LinkStore = flux.getStore("LinkStore");
+    this.LinkActions = flux.getActions("LinkActions");
+
+    this.state = {
+      links: this.LinkStore.getState()[`${this.props.type}Links`]
+    };
+
+    this.LinkActions.getLinks(this.props.type);
+
+    this.handleStateChange = this.handleStateChange.bind(this);
+    this.buildLinks = this.buildLinks.bind(this);
+    this.buildErr = this.buildErr.bind(this);
+  }
+
+  componentDidMount(){
+    this.LinkStore.listen(this.handleStateChange);
+  }
+
+  componentWillUnmount(){
+    this.LinkStore.unlisten(this.handleStateChange);
+  }
+
+  handleStateChange(data){
+    console.log(data);
+    this.setState(Object.assign(this.state, { links: data[`${this.props.type}Links`] }));
+  }
+
+  buildLinks(){
+    const data = this.state.links.data ? this.state.links.data.children : [];
+
+    const links = data.map((item, index) => {
+      return (<Link data={item} key={index}/>)
+    });
 
     return (
-      <ul>
-        {links}
-      </ul>
+      <ul> { links } </ul>
+    );
+  }
+
+  buildErr(){
+    return (
+      <div>
+        <p>Very funny error message.</p>
+      </div>
+    )
+  }
+
+  render(){
+    const content = this.state.links.error ? this.buildErr() : this.buildLinks();
+
+    return (
+      <div>
+        { this.props.children }
+
+        { content }
+      </div>
     )
   }
 }
 
 Links.contextTypes = {
   flux: React.PropTypes.object
-}
+};
 
 export default Links
