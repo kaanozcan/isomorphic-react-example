a67c023b613b6c42c47e28a9c4c8a377ed065ef1
root <knzcnn@gmail.com>
2016-02-10 00:54:53 +0200

    added for displaying error message example

--- src/common/components/JavascriptLinks.jsx
+++ src/common/components/JavascriptLinks.jsx
@@ -2,9 +2,6 @@ import React, { Component } from "react"
 import Links from "./Links"
 
 class JavascriptLinks extends Component {
-  constructor(config){
-    super(config);
-  }
   render(){
       return (
         <div>
@@ -13,6 +10,9 @@ class JavascriptLinks extends Component {
           </Links>
           <Links type="new">
             <h3>New Javascript Links</h3>
+          </Links>
+          <Links type="gimmeerr">
+            <h3>Nice Little Error Here</h3>
           </Links>
         </div>
       );
