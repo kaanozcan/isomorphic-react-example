436dce47918b0e9dcf4b50e915858caf6101823d
root <knzcnn@gmail.com>
2016-03-14 01:46:52 +0200

    action constants

---
+++ src/common/flux/constants/index.js
@@ -0,0 +1,7 @@
+const actionConstants = {
+  "FETCH_LINKS": "FETCH_LINKS",
+  "FETCH_LINKS_SUCCESS": "FETCH_LINKS_SUCCESS",
+  "FETCH_LINKS_ERROR": "FETCH_LINKS_ERROR"
+}
+
+export { actionConstants }
