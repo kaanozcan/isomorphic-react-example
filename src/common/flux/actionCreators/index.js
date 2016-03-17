7895cef33c312d2e7e3ed4d4e09f4cbb92a492b2
root <knzcnn@gmail.com>
2016-03-18 01:04:16 +0200

    changed action names / handled request error

--- src/common/flux/actionCreators/index.js
+++ src/common/flux/actionCreators/index.js
@@ -1,23 +1,34 @@
-import { actionConstants } from "../constants"
+import { linkActions } from "../constants"
 
 export function fetchLinks (subreddit, type){
   return (dispatch) => {
     const url = `https://www.reddit.com/r/${subreddit}/${type}.json` ;
     fetch(url)
+      .then(response => {
+        if(response.status >= 200 && response.status < 300){
+          return response;
+        } else {
+          let error = new Error(response.statusText);
+          error.response = response;
+          throw error;
+        }
+
+        return response.json()
+      })
       .then(response => response.json())
       .then(json =>
         dispatch(
           fetchLinksSuccess(subreddit, type, json
         )
       ),
-      err => dispatch(fetchLinksError(subreddit, type, err))
+        err => dispatch(fetchLinksError(subreddit, type, err))
     )
   }
 }
 
 export function fetchLinksSuccess (subreddit, type, data){
   return {
-    type: actionConstants.FETCH_LINKS_SUCCESS,
+    type: linkActions.FETCH_SUCCESS,
     data: {
       subreddit,
       type,
@@ -28,7 +39,7 @@ export function fetchLinksSuccess (subreddit, type, data){
 
 export function fetchLinksError (subreddit, type, error){
   return {
-    type: actionConstants.FETCH_LINKS_ERROR,
+    type: linkActions.FETCH_ERROR,
     data: {
       subreddit,
       type,
