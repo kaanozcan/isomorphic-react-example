bcffe68aa1e13f2a90c66d4ec97f62f6bcd3742a
root <knzcnn@gmail.com>
2016-04-01 01:22:31 +0300

    imported constants

--- src/common/flux/reducers/linkReducer.js
+++ src/common/flux/reducers/linkReducer.js
@@ -1,10 +1,10 @@
-import { linkActions } from "../constants"
+import constants from "../constants"
 import { actionCreators } from "../actionCreators"
 import fetch from "isomorphic-fetch"
 
 export default function (state, action){
   switch (action.type) {
-    case linkActions.FETCH_SUCCESS:
+    case constants.linkActions.FETCH_SUCCESS:
       return Object.assign({}, state, {
         [action.data.subreddit + action.data.type]: {
           error: false,
@@ -12,7 +12,7 @@ export default function (state, action){
         }
       });
     break;
-    case linkActions.FETCH_ERROR:
+    case constants.linkActions.FETCH_ERROR:
       return Object.assign({}, state, {
         [action.data.subreddit + action.data.type]: {
             error: action.data.error,
