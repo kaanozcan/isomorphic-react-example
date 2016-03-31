eadf621f92e8f2b71e93ea6da2802e1c09ae761c
root <knzcnn@gmail.com>
2016-04-01 01:21:10 +0300

    dispatching all requests

--- src/server/renderer.js
+++ src/server/renderer.js
@@ -2,32 +2,36 @@ import React from "react"
 import { renderToString } from "react-dom/server"
 import { createStore, applyMiddleware} from "redux"
 import linkReducer from "../common/flux/reducers/linkReducer.js"
-import thunk from "redux-thunk"
+
+import resolver from "../common/flux/middlewares/reduxResolver.js"
+
 import App from "../common/components/App.jsx"
 
-
-
-
 export default function (req, res, next){
-  console.log(linksReducer);
-  const store = createStore(
+  const resolverMiddleware = resolver();
+
+  let store = createStore(
     linkReducer,
     {javascript: {new: {}, hot: {}}},
-    applyMiddleware(thunk)
+    applyMiddleware(resolverMiddleware.resolver)
   );
 
   let markup = renderToString(<App store={store} path={req.url}/>);
 
-  res.status(200).send(
-      `<html>
-      <head>
-        <script src="/assets/bundle.js"></script>
-      </head>
-      <body>
-        <div id="root">
-          ${markup}
-        </div>
-      </body>
-    </html>`
-  );
+  resolverMiddleware.dispatchAll().then(() => {
+    markup = renderToString(<App store={store} path={req.url}/>);
+
+    res.status(200).send(
+        `<html>
+        <head>
+          <script src="/assets/bundle.js"></script>
+        </head>
+        <body>
+          <div id="root">
+            ${markup}
+          </div>
+        </body>
+      </html>`
+    );
+  });
 }
