2b751c83add5826d0a5e4b7fbcf94f63b2ce8c71
root <knzcnn@gmail.com>
2016-04-01 01:15:13 +0300

    completed resolver

--- src/common/flux/middlewares/reduxResolver.js
+++ src/common/flux/middlewares/reduxResolver.js
@@ -1,7 +1,26 @@
-const middleware = store => next => action => {
-  if(typeof action === "function"){
+import process from "process"
 
+let requests = [];
+
+const middleware = store => next => action => {
+  if(typeof action === "function") {
+    const promise = action(next);
+
+    if(process.browser){
+      new Promise(promise);
+    } else {
+      requests.push(promise);
+    }
+  } else {
+    next(action);
   }
 }
 
-export default middleware;
+export default function () {
+  return {
+    dispatchAll: function () {
+      return Promise.all(requests.map(promise => new Promise(promise)));
+    },
+    resolver: middleware
+  }
+};
