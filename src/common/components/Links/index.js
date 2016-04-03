cc014c7e40aaeb2036da301326945ad08dbc8f2f
root <knzcnn@gmail.com>
2016-04-04 02:48:16 +0300

    using connect

--- src/common/components/Links/index.js
+++ src/common/components/Links/index.js
@@ -1,39 +1,15 @@
 import React, { Component } from "react"
 import { fetchLinks } from "../../flux/actionCreators"
+import { connect } from "react-redux"
 import Link from "./Link"
 
 class Links extends Component {
-  constructor(config, context){
-    super(config);
-
-    const store = this.store = context.store;
-
-    this.state = store.getState();
-
-    this.handleStateChange = this.handleStateChange.bind(this);
-    this.buildLinks = this.buildLinks.bind(this);
-    this.buildErr = this.buildErr.bind(this);
+  componentWillMount(){
+    console.log("i got called", this.props.type)
+    this.props.fetchLinks();
   }
-
-  componentDidMount() {
-    this.unsubscribe = this.store.subscribe(this.handleStateChange);
-    this.store.dispatch(fetchLinks("javascript", this.props.type));
-  }
-
-  componentWillUnmount(){
-    this.unsubscribe();
-  }
-
-  handleStateChange(data){
-    this.setState(
-      Object.assign({}, this.state, this.store.getState())
-    );
-  }
-
   buildLinks(){
-    const data = this.state["javascript" + this.props.type] && this.state["javascript" + this.props.type].data ? this.state["javascript" + this.props.type].data : [];
-
-    const links = data.map((item, index) => {
+    const links = this.props.data.map((item, index) => {
       return (<Link data={item} key={index}/>)
     });
 
@@ -51,8 +27,8 @@ class Links extends Component {
   }
 
   render(){
-    const err = this.state["javascript" + this.props.type] && this.state["javascript" + this.props.type].error ? this.state["javascript" + this.props.type].error : false;
-    const content = err ? this.buildErr() : this.buildLinks();
+    console.log("render links", this.props.type);
+    const content = this.props.error ? this.buildErr() : this.buildLinks();
 
     return (
       <div>
@@ -64,8 +40,23 @@ class Links extends Component {
   }
 }
 
-Links.contextTypes = {
-  store: React.PropTypes.object
-};
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    fetchLinks: () => {
+        dispatch(fetchLinks("javascript", ownProps.type));
+    }
+  }
+}
 
-export default Links
+const mapStateToProps = (state, ownProps) => {
+  const data = state.has("javascript" + ownProps.type) && state.get("javascript" + ownProps.type).data || []
+  const error = state.has("javascript" + ownProps.type) && state.get("javascript" + ownProps.type).error || false;
+  console.log("stateToProps", data);
+
+  return {
+    data: data,
+    error: error
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Links)
