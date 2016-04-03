03ffc60ecd3b16fcb3b78aa09c508cf9f0be88a5
root <knzcnn@gmail.com>
2016-04-04 02:46:40 +0300

    server side render

--- src/server/renderer.js
+++ src/server/renderer.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { renderToString } from "react-dom/server"
 import { createStore, applyMiddleware} from "redux"
+import { Provider } from "react-redux"
+import { Map } from "immutable"
 import linkReducer from "../common/flux/reducers/linkReducer.js"
 
 import resolver from "../common/flux/middlewares/reduxResolver.js"
@@ -12,15 +14,38 @@ export default function (req, res, next){
 
   let store = createStore(
     linkReducer,
-    {javascript: {new: {}, hot: {}}},
     applyMiddleware(resolverMiddleware.resolver)
   );
 
-  let markup = renderToString(<App store={store} path={req.url}/>);
+  let markup = renderToString(<Provider store={store}><App path={req.url}/></Provider>);
+  console.log("first markup", markup);
+/*
+  res.status(200).send(
+      `<html>
+      <head>
+        <script src="/assets/bundle.js"></script>
+      </head>
+      <body>
+        <div id="root">
+          ${markup}
+        </div>
+      </body>
+    </html>`
+  );
+*/
 
   resolverMiddleware.dispatchAll().then(() => {
-    markup = renderToString(<App store={store} path={req.url}/>);
+    console.log("bitch wuno");
+    console.log("store.getState()", store.getState());
 
+    const finalStore = createStore(
+      linkReducer,
+      store.getState(),
+      applyMiddleware(resolverMiddleware.resolver)
+    );
+
+    markup = renderToString(<Provider store={finalStore}><App path={req.url}/></Provider>);
+    console.log("maybe here?");
     res.status(200).send(
         `<html>
         <head>
@@ -34,4 +59,5 @@ export default function (req, res, next){
       </html>`
     );
   });
+  
 }
