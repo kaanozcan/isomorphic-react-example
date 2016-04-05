bcdc903af8641e26a21c5cf0968da8ca55ee48a3
Kaan Özcan <knzcnn@gmail.com>
2016-04-05 09:30:38 +0300

    renderer

---
+++ src/server/renderer.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { renderToString } from "react-dom/server"
+import { createStore, applyMiddleware} from "redux"
+import { Provider } from "react-redux"
+import { Map } from "immutable"
+import linkReducer from "../common/flux/reducers/linkReducer.js"
+
+import resolver from "../common/flux/middlewares/reduxResolver.js"
+
+import App from "../common/components/App.jsx"
+
+export default function (req, res, next){
+  const resolverMiddleware = resolver();
+
+  let store = createStore(
+    linkReducer,
+    applyMiddleware(resolverMiddleware.resolver)
+  );
+
+  let markup = renderToString(<Provider store={store}><App path={req.url}/></Provider>);
+
+  resolverMiddleware.dispatchAll().then(() => {
+    markup = renderToString(<Provider store={store}><App path={req.url}/></Provider>);
+
+    res.status(200).send(
+        `<html>
+        <head>
+          <script src="/assets/bundle.js"></script>
+        </head>
+        <body>
+          <div id="root">
+            ${markup}
+          </div>
+        </body>
+      </html>`
+    );
+  });
+
+}
